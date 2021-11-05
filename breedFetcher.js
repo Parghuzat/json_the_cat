2886acd1921689da08b97ffc601e1f0da77c12b4
Parghuzat <paguzati@hotmail.com>
2021-11-04 20:59:04 -0700

    index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
 const request = require('request');
-const breed = process.argv[2];
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
-request (url, (error, response, body) => {
-	if (error !== undefined){
-		console.log()
-	}
-	else if (body == '[]') {
-		console.log("No result found!");
-	}
-	else {
-	const data = JSON.parse(body);
-	console.log(data[0].description);
-	}
-})
 
+
+
+const fetchBreedDescription = function (breedName, callback) {
+
+	let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+	request (url, (error, response, body) => {
+		if (error !== null){
+			callback(error, undefined);
+		}
+		else if (body == '[]') {
+			callback(undefined, 'Nothing Found');
+		}
+		else {
+			const data = JSON.parse(body);
+			callback(undefined, data[0].description);
+		}
+	})
+}
+module.exports = fetchBreedDescription;
